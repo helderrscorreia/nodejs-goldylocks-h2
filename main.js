3b4c0032552744a3b9cf7908a8ebabd62c0c43c5
Hélder Correia <helderrscorreia@gmail.com>
2020-08-10 16:40:35 +0100

    Customers importation

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const mysql = require('mysql');
 // load configuration file
 const configuration = require('./config.json');
 
+// instantiate Goldylocks API requests
+const gl = new Goldylocks();
+gl.setup(configuration.goldylocks_alias, configuration.goldylocks_api_key);
+
 // create mysql/mariadb connection
 const sql = mysql.createConnection({
     host: configuration.h2_database_host,
@@ -13,8 +17,8 @@ const sql = mysql.createConnection({
     database: configuration.h2_database_name
 });
 
-// process customers
-getAllCustomers = () => {
+// get customers
+const getAllCustomers = () => {
     return new Promise((resolve, reject) => {
         sql.query(`SELECT *
                    FROM entidades`, (err, res) => {
@@ -27,10 +31,10 @@ getAllCustomers = () => {
     });
 }
 
-// process customers
-getPendingDocuments = () => {
+// get pending documents
+const getPendingDocuments = () => {
     return new Promise((resolve, reject) => {
-        sql.query(`SELECT *
+        sql.query(`SELECT entidadesmovimentos.*, DATE_FORMAT(entidadesmovimentos.DATACRIACAO,"%Y-%m-%d") as data_criacao, TIME_FORMAT(entidadesmovimentos.HORACRIACAO,"%H:%i:%s")  AS hora_criacao
                    FROM entidadesmovimentos
                    WHERE entidadesmovimentos.VALORPENDENTE <> 0`, (err, res) => {
             if (!err) {
@@ -41,4 +45,150 @@ getPendingDocuments = () => {
         });
     });
 }
+
+/**
+ * process data array throug async iteration
+ * @type {{processFunction: null, process: process, data: [], start: start, callback: null}}
+ */
+let processData = {
+    data: [],
+    processFunction: null,
+    callback: null,
+
+    // setup and start data processing
+    start: (data, processFunction, callback) => {
+        processData.callback = callback;
+        processData.data = data;
+        processData.processFunction = processFunction;
+        processData.process();
+    },
+    process: () => {
+        if (processData.data.length > 0) {
+            // get first data position
+            let data = processData.data[0];
+
+            // clear read data in the array
+            processData.data.shift();
+
+            // process each item
+            processData.processFunction(data, () => {
+                processData.process();
+            });
+        } else {
+            // data array is empty, its time to callback
+            if (processData.callback) processData.callback();
+        }
+    }
+}
+
+/**
+ * create customer at Goldylocks
+ * @param customerData
+ * @param callback
+ */
+const createCustomerGoldylocks = (customerData, callback) => {
+    console.log(customerData.NOME)
+    gl.request("gerircliente", {}, {
+        nome: customerData.NOME,
+        morada: customerData.MORADA,
+        cp: `${customerData.CODIGOPOSTAL} ${customerData.LOCALIDADE}`,
+        nif: customerData.NIF,
+        observacoes: customerData.OBS,
+        linha: 1,
+        imposto: 'IVA 23%',
+        codigo_pais: customerData.PAIS,
+        pais: 'Portugal',
+        zona_fiscal: "PRT",
+        telefone: customerData.TELEFONE,
+        telemovel: customerData.TELEMOVEL
+    }, callback);
+}
+
+/**
+ * create Goldylocks ledger document
+ * @param documentData
+ * @param callback
+ */
+const createPendingDocumentGoldylocks = (documentData, callback) => {
+
+    getCustomerDataByVAT(documentData.NIF, glCustomerData => {
+        console.log({
+            idcliente: glCustomerData.id,
+            inp_referencia: documentData.KEY,
+            tipo_documento: configuration.ledger_document_type_id,
+            inp_data_documento: `${documentData.data_criacao} ${documentData.hora_criacao}`
+        });
+        gl.request("criardocumento", {}, {
+            idcliente: glCustomerData.id,
+            inp_referencia: documentData.KEY,
+            tipo_documento: configuration.ledger_document_type_id,
+            inp_data_documento: `${documentData.data_criacao} ${documentData.hora_criacao}`
+        }, newDocumentID => {
+            console.log(newDocumentID);
+        });
+    });
+}
+
+/**
+ * search Goldylocks customer data using VAT number
+ * @param customerVAT
+ * @param callback
+ */
+const getCustomerDataByVAT = (customerVAT, callback) => {
+    gl.request("clientes", {
+        "minimal": "1",
+        "p": customerVAT
+    }, {}, customerData => {
+        if (callback && customerData.length > 0) {
+            callback(customerData[0]);
+        } else if (callback) {
+            callback(false);
+        } else {
+            return false;
+        }
+    });
+}
+
+/**
+ * start the process to import customers
+ * @param callback
+ */
+const processCustomers = (callback) => {
+    if (configuration.import_customers) {
+        getAllCustomers()
+            .then(customers => {
+                processData.start(customers, createCustomerGoldylocks, () => {
+                    if (callback) callback();
+                });
+            })
+            .catch(err => console.error(err));
+    } else {
+        if (callback) callback();
+    }
+}
+
+/**
+ * start the process to import ledger documents
+ * @param callback
+ */
+const processPendingDocuments = (callback) => {
+    console.log(`Process pending documents...`);
+    if (configuration.import_ledgers) {
+        getPendingDocuments()
+            .then(pendingDocuments => {
+                processData.start(pendingDocuments, createPendingDocumentGoldylocks, callback);
+            })
+            .catch(err => console.error(err));
+    } else {
+        if (callback) callback();
+    }
+}
+
+// main
+processCustomers(() => {
+    processPendingDocuments(() => {
+        sql.end();
+    })
+})
+
 
